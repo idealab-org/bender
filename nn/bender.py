58ed33ebec69eab54ce020d8c7f41e8e32cbc088
Naren Dasan <naren@narendasan.com>
2017-10-19 14:52:52 -0500

    ML skeleton

---
+++ nn/bender.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+import numpy as np
+
+
+
+
+def main():
+    print("placeholder")
+
+
+if __name__ == "__main__":
+    main()
---
+++ nn/predictor/predictor.py
@@ -0,0 +1,26 @@
+import torch
+import torchvision
+import numpy as np
+
+
+def load_stroop_results():
+    '''Import collected data from store'''
+
+def train():
+    '''
+    Train the predictor to perceive Stroop tests in the same way people do.
+    Used as a simulation tool for the generator (bender)
+    '''
+
+
+def infer(img):
+    '''
+    Generate a prediction of how a person will react to a given stroop test
+    '''
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
